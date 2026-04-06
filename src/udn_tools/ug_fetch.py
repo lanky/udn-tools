53368be949cbf3427a63042f849bc798abf87b4a
Stuart Sears <stuart@sjsears.com>
2026-04-06 10:55:17 +0100

    Add ug_to_crd script entry point, rename ug_fetch_new

--- src/udn_tools/ug_fetch.py
+++ src/udn_tools/ug_fetch.py
@@ -5,6 +5,7 @@ import argparse
 import json
 import re
 import sys
+from datetime import datetime
 from pathlib import Path
 
 import jmespath
@@ -14,13 +15,22 @@ from selenium import webdriver
 # naive pattern matching for chords
 CHORD = re.compile(r"[A-G][adgijmnsu0-9#b+-\/\*A-G]*")
 
+
 def parse_cmdline(argv: list[str] = sys.argv[1:]) -> argparse.Namespace:
     """Process commandline arguments."""
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
 
     parser.add_argument("source", help="URL to  Ultimate Guitar chordsheet.")
+    parser.add_argument(
+        "-s",
+        "--save-source",
+        action="store_true",
+        default=False,
+        help="Save the HTML source (for debugging)",
+    )
 
     return parser.parse_args()
+
 
 def fetch_source(url: str) -> str:
     """Fetch the page source from UG via selenium."""
@@ -30,10 +40,11 @@ def fetch_source(url: str) -> str:
     browser = webdriver.Firefox(options=browser_opts)
 
     browser.get(url)
-    
+
     return browser.page_source
 
-def extract_tab(html: str) -> str|None:
+
+def extract_tab(html: str, save: bool = False) -> str | None:
     """Parse the html to extract the tab/chords."""
     soup = bs(html, features="lxml")
     content = soup.find("pre", {"class": "k_vI3 KLhHx fGc1h"})
@@ -43,42 +54,46 @@ def extract_tab(html: str) -> str|None:
     print(meta)
 
     if content is not None:
-        if footer := content.find("div", { "class": "d8c-l"}):
+        if footer := content.find("div", {"class": "d8c-l"}):
             footer.decompose()
 
         lines = content.text.splitlines()
 
         # header = "Unknown Title - Unknown Artist"
         if meta:
-            header = f"{meta['title']} - { meta['artist']}"
+            header = f"{meta['title']} - {meta['artist']}"
         else:
             header = "Unknown Title - Unknown Artist"
 
         lines.insert(0, header)
         print(lines)
 
-        dest = Path(header.lower().replace(" ","_")).with_suffix(".crd")
-        
+        dest = Path(header.lower().replace(" ", "_")).with_suffix(".crd")
+
         dest.write_text("\n".join(lines))
         return content.text
     return content
 
+
 def parse_meta(soup: bs) -> dict:
     """Parse out metadata blobs."""
     # metadata is a JSONish blob stored as a script
-    meta = soup.find("script",{ "type": "application/ld+json"})
+    meta = soup.find("script", {"type": "application/ld+json"})
     if meta:
         metadata = json.loads(meta.text.replace("@", ""))
-        return jmespath.search("""
+        return jmespath.search(
+            """
         {
         title: name,
         artist: byArtist.name,
         url: url
         }
         """,
-        metadata)
+            metadata,
+        )
     else:
         return {}
+
 
 def main():
     """Run all the things."""
@@ -87,6 +102,9 @@ def main():
     print(f"Fetching source from {opts.source}")
 
     src = fetch_source(opts.source)
+
+    if opts.save_source:
+        Path(f"{opts.source.split('/')[-1]}.html").write_text(src)
 
     print("Attempting to parse sources")
 
